8695defdbe53db10cd9d13ad8f52527c83e0c8fd
Yaoyu Hu <huyaoyu1986@gmail.com>
2020-03-21 14:10:31 -0400

    Add normal flipping to NormalEstimation.

--- src/NormalEstimation.cpp
+++ src/NormalEstimation.cpp
@@ -20,8 +20,8 @@
 #include <pcl/point_types.h>
 #include <pcl/common/centroid.h>
 #include <pcl/common/io.h> // From copyPointCloud().
+#include <pcl/common/point_tests.h>
 #include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/surface/mls.h>
 
 #include "Args/Args.hpp"
@@ -51,20 +51,31 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
 class Args
 {
 public:
-    Args(): polyOrder(2), radius(0.05) {}
+    Args();
     ~Args() = default;
 
-    bool validate(void) {
+    bool validate() {
         bool flag = true;
+
+        bool flagPCType = validate_string_with_trimming(computeType, validComputeTypeList);
+
+        if ( !flagPCType ) {
+            std::cout << "Validate computation type: " << computeType << std::endl;
+            std::cout << "Valid types are: " << std::endl;
+            show_vector_as_list(validComputeTypeList);
+        }
+
+        flag = flag && flagPCType;
 
         return flag;
     }
 
     friend std::ostream& operator<<(std::ostream& out, const Args& args) {
-        out << args.AS_IN_FILE << ": " << args.inFile << std::endl;
-        out << args.AS_OUT_FILE << ": " << args.outFile << std::endl;
-        out << args.AS_POLY_ORDER << ": " << args.polyOrder << std::endl;
-        out << args.AS_RADIUS << ": " << args.radius << std::endl;
+        out << Args::AS_IN_FILE << ": " << args.inFile << std::endl;
+        out << Args::AS_OUT_FILE << ": " << args.outFile << std::endl;
+        out << Args::AS_COMPUTE_TYPE << ": " << args.computeType << std::endl;
+        out << Args::AS_POLY_ORDER << ": " << args.polyOrder << std::endl;
+        out << Args::AS_RADIUS << ": " << args.radius << std::endl;
 
         return out;
     }
@@ -72,20 +83,35 @@ public:
 public:
     static const std::string AS_IN_FILE; // AS stands for argument string
     static const std::string AS_OUT_FILE;
+    static const std::string AS_COMPUTE_TYPE;
     static const std::string AS_POLY_ORDER;
     static const std::string AS_RADIUS;
+
+    static const std::string VAS_COMPUTE_TYPE_MLS;
+    static const std::string VAS_COMPUTE_TYPE_NE;
 
 public:
     std::string inFile; // The input point cloud file.
     std::string outFile; // The output file.
+    std::string computeType; // The type of computation performed for estimating the normal.
+    std::vector<std::string> validComputeTypeList;
     int polyOrder; // The order of the polynomial.
     double radius; // The search radius.
 };
 
-const std::string Args::AS_IN_FILE    = "infile";
-const std::string Args::AS_OUT_FILE   = "outfile";
-const std::string Args::AS_POLY_ORDER = "poly-order";
-const std::string Args::AS_RADIUS     = "radius";
+const std::string Args::AS_IN_FILE      = "infile";
+const std::string Args::AS_OUT_FILE     = "outfile";
+const std::string Args::AS_COMPUTE_TYPE = "compute-type";
+const std::string Args::AS_POLY_ORDER   = "poly-order";
+const std::string Args::AS_RADIUS       = "radius";
+
+const std::string Args::VAS_COMPUTE_TYPE_MLS = "MLS";
+const std::string Args::VAS_COMPUTE_TYPE_NE  = "NE";
+
+Args::Args()
+: polyOrder(2), radius(0.05),
+  computeType(VAS_COMPUTE_TYPE_MLS), validComputeTypeList({ VAS_COMPUTE_TYPE_MLS, VAS_COMPUTE_TYPE_NE })
+{ }
 
 static void parse_args(int argc, char* argv[], Args& args) {
 
@@ -97,6 +123,7 @@ static void parse_args(int argc, char* argv[], Args& args) {
                 ("help", "Produce help message.")
                 (Args::AS_IN_FILE.c_str(), bpo::value< std::string >(&(args.inFile))->required(), "Input file.")
                 (Args::AS_OUT_FILE.c_str(), bpo::value< std::string >(&(args.outFile))->required(), "Output file.")
+                (Args::AS_COMPUTE_TYPE.c_str(), bpo::value< std::string >(&args.computeType)->default_value(Args::VAS_COMPUTE_TYPE_MLS), "The computation type, MLS or NE.")
                 (Args::AS_POLY_ORDER.c_str(), bpo::value< int >(&args.polyOrder)->default_value(2), "The order of the polynomial.")
                 (Args::AS_RADIUS.c_str(), bpo::value<double>(&args.radius)->default_value(0.05), "The search radius.");
 
@@ -113,6 +140,13 @@ static void parse_args(int argc, char* argv[], Args& args) {
         std::cout << e.what() << std::endl;
         throw(e);
     }
+
+    // Validate the point cloud type specification.
+    if ( !args.validate() ) {
+        std::stringstream ss;
+        ss << "args.validate() returns false. ";
+        throw(std::runtime_error(ss.str()));
+    }
 }
 
 template <typename T>
@@ -120,13 +154,13 @@ static void read_point_cloud(const std::string& fn, typename pcl::PointCloud<T>:
     // ========== Read the point cloud from the file. ==========
     std::cout << "Loading points from " << fn << " ... " << std::endl;
 
-    QUICK_TIME_START(teReadPointCloud);
+    QUICK_TIME_START(teReadPointCloud)
     if ( pcl::io::loadPLYFile<T>(fn, *pOutCloud) == -1 ) {
         std::stringstream ss;
         ss << "Failed to read: " << fn;
         throw( std::runtime_error(ss.str()) );
     }
-    QUICK_TIME_END(teReadPointCloud);
+    QUICK_TIME_END(teReadPointCloud)
 
     std::cout << pOutCloud->size() << " points loaded in " << teReadPointCloud << "ms. " << std::endl;
 }
@@ -135,7 +169,7 @@ template <typename T>
 static void compute_normal(const typename pcl::PointCloud<T>::Ptr& pInput,
         pcl::PointCloud<pcl::Normal>::Ptr& pOutput,
         const double radius=0.05) {
-    QUICK_TIME_START(te);
+    QUICK_TIME_START(te)
 
     // Compute the centroid of the point cloud.
     Eigen::Vector4f centroid;
@@ -156,7 +190,7 @@ static void compute_normal(const typename pcl::PointCloud<T>::Ptr& pInput,
     pcl::PointCloud<pcl::Normal>::Ptr temp ( new pcl::PointCloud<pcl::Normal> );
     ne.compute(*pOutput);
 
-    QUICK_TIME_END(te);
+    QUICK_TIME_END(te)
 
     std::cout << "Compute normal in " << te << "ms. " << std::endl;
 }
@@ -165,7 +199,7 @@ template <typename inT, typename outT>
 static void moving_least_square(const typename pcl::PointCloud<inT>::Ptr& pInput,
         typename pcl::PointCloud<outT>::Ptr& pOutput,
         const int order, const double radius, const bool flagNormal=true ) {
-    QUICK_TIME_START(te);
+    QUICK_TIME_START(te)
 
     // Create a KD-Tree.
     typename pcl::search::KdTree<inT>::Ptr tree ( new pcl::search::KdTree<inT> );
@@ -180,12 +214,51 @@ static void moving_least_square(const typename pcl::PointCloud<inT>::Ptr& pInput
 
     mls.process(*pOutput);
 
-    QUICK_TIME_END(te);
+    QUICK_TIME_END(te)
 
     std::cout << "Moving least square in " << te << "ms. " << std::endl;
 }
 
+/**
+ * Flip the normal stored in a pcl::PointNormal typed point cloud.
+ *
+ * @param pInput The input point cloud.
+ * @param vx The x coordinate of the view point.
+ * @param vy The y coordinate of the view point.
+ * @param vz The z coordinate of the view point.
+ */
+static void flip_normal_inplace( pcl::PointCloud<pcl::PointNormal>::Ptr& pInput,
+                          const float vx, const float vy, const float vz ) {
+    QUICK_TIME_START(te)
+
+    std::cout << "Start flipping the normal. " << std::endl;
+
+    // Loop over all the points in the point cloud.
+    for ( auto iter = pInput->begin(); iter != pInput->end(); iter++ ) {
+        if ( pcl::isFinite(*iter) ) {
+            pcl::flipNormalTowardsViewpoint( *iter, vx, vy, vz,
+                    (*iter).normal_x, (*iter).normal_y, (*iter).normal_z );
+        }
+    }
+
+    QUICK_TIME_END(te)
+    std::cout << "Execute flip_normal_inplace() in " << te << "ms. " << std::endl;
+}
+
+/**
+ * Flip the normal stored in a pcl::PointNormal typed point cloud.
+ *
+ * @param pInput The input point cloud.
+ * @param vp The viewing point, with the last element being 1.
+ */
+static void flip_normal_inplace( pcl::PointCloud<pcl::PointNormal>::Ptr& pInput,
+        const Eigen::Vector4f& vp ) {
+    flip_normal_inplace(pInput, vp(0), vp(1), vp(2));
+}
+
 int main(int argc, char* argv[]) {
+    QUICK_TIME_START(teMain)
+
     std::cout << "Hello, NormalEstimation!" << std::endl;
 
     // Handle the command line.
@@ -203,24 +276,35 @@ int main(int argc, char* argv[]) {
     // Read the point cloud.
     read_point_cloud<P_t>(args.inFile, pInput);
 
-    // Compute normal.
-//    pcl::PointCloud<pcl::Normal>::Ptr pNormal (new pcl::PointCloud<pcl::Normal>);
-//    compute_normal<P_t>(pInput, pNormal, args.radius);
-//
-//    // Copy point cloud.
-//    pcl::PointCloud<pcl::PointXYZ>::Ptr pXYZ (new pcl::PointCloud<pcl::PointXYZ>);
-//    pcl::copyPointCloud(*pInput, *pXYZ);
-//
-//    // Concatenate fields.
-//    pcl::PointCloud<pcl::PointNormal>::Ptr pPN ( new pcl::PointCloud<pcl::PointNormal> );
-//    pcl::concatenateFields(*pXYZ, *pNormal, *pPN);
-//
+    // The output.
     pcl::PointCloud<pcl::PointNormal>::Ptr pOutput ( new pcl::PointCloud<pcl::PointNormal> );
-//
-//    // Moving least square.
-//    moving_least_square<pcl::PointNormal, pcl::PointNormal>(pPN, pOutput, args.polyOrder, args.radius*0.5);
 
-    moving_least_square<pcl::PointXYZRGB, pcl::PointNormal>(pInput, pOutput, args.polyOrder, args.radius);
+    // Compute normal.
+    if ( args.computeType == Args::VAS_COMPUTE_TYPE_NE ) {
+        pcl::PointCloud<pcl::Normal>::Ptr pNormal (new pcl::PointCloud<pcl::Normal>);
+        compute_normal<P_t>(pInput, pNormal, args.radius);
+
+        // Copy point cloud.
+        pcl::PointCloud<pcl::PointXYZ>::Ptr pXYZ (new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::copyPointCloud(*pInput, *pXYZ);
+
+        // Concatenate fields.
+        pcl::concatenateFields(*pXYZ, *pNormal, *pOutput);
+    } else if ( args.computeType == Args::VAS_COMPUTE_TYPE_MLS ) {
+        // Moving least square.
+        moving_least_square<pcl::PointXYZRGB, pcl::PointNormal>(pInput, pOutput, args.polyOrder, args.radius);
+
+        // Flip the normal.
+        // Compute the centroid of the point cloud.
+        Eigen::Vector4f centroid;
+        pcl::compute3DCentroid(*pInput, centroid);
+        flip_normal_inplace(pOutput, centroid);
+    } else {
+        // Should never be here.
+        std::stringstream ss;
+        ss << "Unexpected args.computeType value " << args.computeType;
+        throw(std::runtime_error(ss.str()));
+    }
 
     // Get the file parts.
     auto parts = get_file_parts(args.outFile);
@@ -229,13 +313,17 @@ int main(int argc, char* argv[]) {
     test_directory( parts[0] );
 
     // Save the point cloud.
-    QUICK_TIME_START(teWrite);
+    QUICK_TIME_START(teWrite)
     pcl::PLYWriter writer;
     std::cout << "Saving the filtered point cloud." << std::endl;
     writer.write(args.outFile, *pOutput, true, false);
-    QUICK_TIME_END(teWrite);
+    QUICK_TIME_END(teWrite)
 
     std::cout << "Write point cloud in " << teWrite << " ms. " << std::endl;
+
+    QUICK_TIME_END(teMain)
+
+    std::cout << "Total time is " << teMain << "ms. " << std::endl;
 
     return 0;
 }
