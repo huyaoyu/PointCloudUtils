6a3f07b552a5f6f06cdc7bf27bad52193e89b52d
Yaoyu Hu <huyaoyu1986@gmail.com>
2020-03-29 15:47:32 -0400

    Before add submoudle for CSV parsing.

---
+++ includes/CameraGeometry/CameraPose2PCL.hpp
@@ -0,0 +1,23 @@
+//
+// Created by yaoyu on 3/29/20.
+//
+
+#ifndef POINTCLOUDUTILS_CAMERAPOSE2PCL_HPP
+#define POINTCLOUDUTILS_CAMERAPOSE2PCL_HPP
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <Eigen/Dense>
+
+#include <pcl/point_types.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include "PCCommon/IO.hpp"
+#include "Profiling/SimpleTime.hpp"
+
+
+
+#endif //POINTCLOUDUTILS_CAMERAPOSE2PCL_HPP
---
+++ includes/CameraGeometry/IO.hpp
@@ -0,0 +1,20 @@
+//
+// Created by yaoyu on 3/29/20.
+//
+
+#ifndef POINTCLOUDUTILS_IO_HPP
+#define POINTCLOUDUTILS_IO_HPP
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <Eigen/Dense>
+
+#include "Profiling/SimpleTime.hpp"
+
+
+
+#endif //POINTCLOUDUTILS_IO_HPP
